978106932f23c0019694c0aae74caa8d6e5b2ee3
artimonist <artimonist@gmail.com>
2025-03-18 16:14:37 +0800

    add overlay for wait cursor

--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
 const { invoke } = window.__TAURI__.core;
 
+let diagram = document.querySelector('simple-diagram');
+let password = document.querySelector('unicode-password');
+let generate = document.querySelector('#generate');
+let overlay = document.getElementById('overlay');
+
 // worker
-var worker = new Worker('./worker/worker.js');
+let worker = new Worker('./worker/worker.js');
 worker.onmessage = function (event) {
   // document.body.style.cursor = 'default';
-  $('*').css('cursor', 'default');
+  // $('*').css('cursor', 'default');
+  overlay.style.display = 'none';
   console.log('Received from worker:', event.data);
 };
 
 // generate 
-let diagram = document.querySelector('simple-diagram');
-let password = document.querySelector('unicode-password');
-let generate = document.querySelector('#generate');
 generate.addEventListener('click', async () => {
   if (diagram.is_empty() || !password.is_valid()) {
     return;
   }
-  $('*').css('cursor', 'wait');
-  document.body.style.cursor = 'wait';
+  // $('*').css('cursor', 'wait');
+  // document.body.style.cursor = 'wait';
+  overlay.style.display = 'block';
   worker.postMessage({
     method: 'simple_init',
     args: [diagram.values, password.value]
